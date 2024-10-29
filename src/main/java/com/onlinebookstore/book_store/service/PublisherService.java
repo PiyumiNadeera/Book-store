4b33b6c4aaa6e37df5e431bbeab7ea42456ef4b5
piyumi <dlpnadeera1234@gmail.com>
2024-10-29 17:58:53 +0530

    Adding publisher service

---
+++ src/main/java/com/onlinebookstore/book_store/service/PublisherService.java
@@ -0,0 +1,15 @@
+package com.onlinebookstore.book_store.service;
+
+import com.onlinebookstore.book_store.entity.Publisher;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface PublisherService {
+    List<Publisher> getAllPublishers();
+    Publisher getPublisherById(Long id);
+    void addPublisher(Publisher publisher);
+    void updatePublisher(Long id, Publisher publisher);
+    void deletePublisher(Long id);
+}
---
+++ src/main/java/com/onlinebookstore/book_store/service/PublisherServiceImpl.java
@@ -0,0 +1,55 @@
+package com.onlinebookstore.book_store.service;
+
+import com.onlinebookstore.book_store.entity.Publisher;
+import com.onlinebookstore.book_store.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PublisherServiceImpl implements PublisherService{
+
+    @Autowired
+    private PublisherRepository publisherRepository;
+
+    public PublisherServiceImpl(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public List<Publisher> getAllPublishers(){
+        return publisherRepository.findAll();
+    }
+
+    @Override
+    public Publisher getPublisherById(Long id){
+        return publisherRepository.findById(id).orElseThrow(null);
+    }
+
+    @Override
+    public void addPublisher(Publisher publisher) {
+        publisherRepository.save(publisher);
+    }
+
+    @Override
+    public void updatePublisher(Long id, Publisher publisher) {
+        Publisher existingPublisher = publisherRepository.findById(id).orElseThrow(null);
+
+        if (existingPublisher != null) {
+            existingPublisher.setPublisherName(publisher.getPublisherName());
+            existingPublisher.setPublisherAddress(publisher.getPublisherAddress());
+
+            publisherRepository.save(existingPublisher);
+        }
+    }
+
+    @Override
+    public void deletePublisher(Long id){
+        Publisher existingPublisher = publisherRepository.findById(id).orElseThrow(null);
+
+        if (existingPublisher != null) {
+            publisherRepository.deleteById(id);
+        }
+    }
+}
